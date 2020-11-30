6803ed5e3a288d49fed4aaa35ac0345ba3900498
leonardo studart <lstudart@pyx4.com>
2020-11-29 23:06:26 -0500

    create a globalStyle with styled components

---
+++ src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import React from 'react'
+import GlobalStyles from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Next - Boilerplate</title>
+        <link rel="shortcut icon" href="/public/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/public/img/icon-512.png" />
+        <meta
+          name="description"
+          content="A simple project starter to work with Typescript, React, NextJS and Styled Components"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
