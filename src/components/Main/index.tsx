a3f140b01c2bad1089c5061ad2f22b0d16694cbe
leonardo studart <lstudart@pyx4.com>
2020-12-02 20:49:47 -0500

    create intiial page

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,5 +1,15 @@
+import * as S from './styles'
+
 export const Main = () => (
-  <main>
-    <h1>react</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" alt="Atom image of React in a Pink Badge" />
+    <S.Title>React with NextJS</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS and Styled Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="developer in front of a screen with code"
+    />
+  </S.Wrapper>
 )
